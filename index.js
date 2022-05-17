0f4c1216a369fb95ba762424da5e8b8e45649bf2
Eric Ekas <eric.ekas@gmail.com>
2022-05-17 11:03:11 -0400

    commit tree addda13f5015d44b546c585f770eeb3233997e3d

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
-// Code your solution in this file!
+
+const returnFirstTwoDrivers = drivers => drivers.slice(0,2)
+
+const returnLastTwoDrivers = drivers => drivers.slice(drivers.length - 2, drivers.length)
+
+let selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers]
+
+function createFareMultiplier(multiplier) {
+    return function multiplyFare(fare) {return fare * multiplier}
+}
+
+const fareDoubler = fare => createFareMultiplier(2)(fare)
+
+const fareTripler = fare => createFareMultiplier(3)(fare)
+
+function selectDifferentDrivers(drivers, driverFunction) {
+    return driverFunction(drivers)
+}
+
